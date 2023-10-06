cba7e6fe84df327ce2f7c58ce4a2a8d56c7b688a
shariyer132 <shariyer132@gmail.com>
2023-10-06 15:53:07 +0600

    added login form

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+export default Login;
